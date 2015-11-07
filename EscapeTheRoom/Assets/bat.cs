1e9d44cbd0e944cae27cb4b70344b975752489de
dianetam523 <diane.tam523@gmail.com>
2015-11-07 04:38:43 -0500

    Weird bad commit with unity lock file permissions. should be okay now

---
+++ EscapeTheRoom/Assets/bat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class bat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
