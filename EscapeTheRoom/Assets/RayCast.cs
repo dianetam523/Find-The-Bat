bd08c7dd02b8868b1378dc286fcda9db62bc0453
Jennifer Zhu <Jennifer Zhu>
2015-11-07 11:06:33 -0500

    added the beginning of raycast files

---
+++ EscapeTheRoom/Assets/RayCast.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 target = GameObject.FindGameObjectWithTag ("CrossHair").transform.position;
+		RayCastHit hit;
+		Physics.Raycast(target, 
+
+	}
+}
